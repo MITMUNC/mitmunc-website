ef1984f10f1dbca352925cbea678efb742c1a2f8
Aron Ricardo Perez-Lopez <perez.aron@gmail.com>
2018-09-16 23:35:51 -0400

    Correct date checking

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,1 +1,2 @@
-REGISTRATION_OPEN = Time.new(2018, 9, 17) < Time.now && Time.now < Time.new(2018, 11, 26)
+# We'll switch time zones from EDT to EST by November.
+REGISTRATION_OPEN = Time.new(2018, 9, 17, 0, 0, 0, "-04:00") < Time.now && Time.now < Time.new(2018, 11, 26, 0, 0, 0, "-05:00")
