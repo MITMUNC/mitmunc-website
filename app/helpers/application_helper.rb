9cf34fccd688fed2bd4a89490695b083923d6e1b
Áron Ricardo Perez-Lopez <perez.aron@gmail.com>
2018-11-24 04:27:22 -0500

    Fix date detection

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def registration_open?
+        return REGISTRATION_START < Time.now && Time.now < REGISTRATION_END
+    end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,3 @@
-# We'll switch time zones from EDT to EST by November.
-REGISTRATION_OPEN = Time.new(2018, 9, 17, 0, 0, 0, "-04:00") < Time.now && Time.now < Time.new(2018, 11, 26, 0, 0, 0, "-05:00")
+# Boston switches time zones from EDT to EST on November 2.
+REGISTRATION_START = Time.new(2018, 9, 17, 0, 0, 0, "-04:00")
+REGISTRATION_END = Time.new(2018, 11, 26, 0, 0, 0, "-05:00")
