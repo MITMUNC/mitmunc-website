880a4239c93175084eef3feb9f46423edeb88902
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2018-12-02 15:14:17 -0500

    Add waitlist open dates

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,5 @@
 # Boston switches time zones from EDT to EST on November 2.
 REGISTRATION_START = Time.new(2018, 9, 17, 0, 0, 0, "-04:00")
 REGISTRATION_END = Time.new(2018, 11, 26, 0, 0, 0, "-05:00")
+WAITLIST_START = Time.new(2018, 12, 2, 0, 0, 0, "-05:00")
+WAITLIST_END = Time.new(2019, 1, 7, 0, 0, 0, "-05:00")
