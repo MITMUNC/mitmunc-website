089c5f03c41ca0bc19770c31cd8ea9ed1f61330f
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2019-02-08 17:19:47 -0500

    Add alert with link to schedule to homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
     def waitlist_open?
         return WAITLIST_START < Time.now && Time.now < WAITLIST_END
     end
+
+    def conference_open?
+        return CONFERENCE_START < Time.now && Time.now < CONFERENCE_END
+    end
 end
