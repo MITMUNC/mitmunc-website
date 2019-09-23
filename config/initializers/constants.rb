1551ab6f43e764c6e30edf2ea96a6b049637844c
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2019-09-23 00:40:21 -0400

    Delay registration opening by a day

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,5 @@
 # Boston switches time zones from EDT to EST on November 3, 2019.
-REGISTRATION_START = Time.new(2019, 9, 23, 0, 0, 0, "-04:00")
+REGISTRATION_START = Time.new(2019, 9, 24, 0, 0, 0, "-04:00")
 REGISTRATION_END = Time.new(2019, 11, 24, 0, 0, 0, "-05:00")
 WAITLIST_START = Time.new(2019, 12, 1, 0, 0, 0, "-05:00")
 WAITLIST_END = Time.new(2020, 1, 5, 0, 0, 0, "-05:00")
