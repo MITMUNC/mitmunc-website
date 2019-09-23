e2848615ab6d6fd8ee5024974b9701357b7cff62
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2019-09-22 20:46:50 -0400

    Update programmed times for MITMUNC XII

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
-# Boston switches time zones from EDT to EST on November 2.
-REGISTRATION_START = Time.new(2018, 9, 17, 0, 0, 0, "-04:00")
-REGISTRATION_END = Time.new(2018, 11, 26, 0, 0, 0, "-05:00")
-WAITLIST_START = Time.new(2018, 12, 2, 0, 0, 0, "-05:00")
-WAITLIST_END = Time.new(2019, 1, 7, 0, 0, 0, "-05:00")
-CONFERENCE_START = Time.new(2019, 2, 8, 17, 0, 0, "-05:00")
-CONFERENCE_END = Time.new(2019, 2, 10, 15, 0, 0, "-05:00")
+# Boston switches time zones from EDT to EST on November 3, 2019.
+REGISTRATION_START = Time.new(2019, 9, 23, 0, 0, 0, "-04:00")
+REGISTRATION_END = Time.new(2019, 11, 24, 0, 0, 0, "-05:00")
+WAITLIST_START = Time.new(2019, 12, 1, 0, 0, 0, "-05:00")
+WAITLIST_END = Time.new(2020, 1, 5, 0, 0, 0, "-05:00")
+CONFERENCE_START = Time.new(2020, 2, 7, 17, 0, 0, "-05:00")
+CONFERENCE_END = Time.new(2020, 2, 9, 15, 0, 0, "-05:00")
