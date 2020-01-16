2ced693d6fb64a663b8bc51fc3a6dddafb08a8b0
Áron Ricardo Perez-Lopez <arpl@mit.edu>
2020-01-16 18:48:19 -0500

    Allow late applications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
     def registration_open?
-        return REGISTRATION_START < Time.now && Time.now < REGISTRATION_END
+        return REGISTRATION_START < Time.now && Time.now < CONFERENCE_START
     end
 
     def waitlist_open?
-        return WAITLIST_START < Time.now && Time.now < WAITLIST_END
+        return WAITLIST_START < Time.now && Time.now < CONFERENCE_START
+    end
+
+    def late_application_penalty_active?
+        return LATE_WAITLIST_START < Time.now && Time.now < CONFERENCE_START
     end
 
     def conference_open?
